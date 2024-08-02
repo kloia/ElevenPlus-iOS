c709fe1f5cde76c5750fc7dbcf7170b881ac4859
Ataberk Kılınçtekin <94781211+AtaberkKilinctekin@users.noreply.github.com>
2024-08-02 16:26:05 +0300

    Initial Commit

---
+++ elevenplus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  elevenplus
+//
+//  Created by İbrahim Ataberk Kılınçtekin on 2.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ elevenplus/elevenplusApp.swift
@@ -0,0 +1,17 @@
+//
+//  elevenplusApp.swift
+//  elevenplus
+//
+//  Created by İbrahim Ataberk Kılınçtekin on 2.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct elevenplusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
